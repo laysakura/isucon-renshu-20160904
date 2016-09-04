66c11872fde2f5e7b752f33d3d3f9c88dbbdfc55
Yasuharu Sawada <yasuharu519@gmail.com>
2016-09-04 11:46:25 +0900

    deploy script の追加

---
+++ fabfile.py
@@ -0,0 +1,37 @@
+from fabric.api import env, run, sudo, cd
+from fabric.contrib.files import exists
+from fabric.decorators import task, runs_once
+from fabric.colors import blue
+from fabric.context_managers import settings
+from functools import wraps
+
+env.user = "isucon"
+env.hosts = ["13.78.93.115"]
+
+@task
+def pull():
+    with cd("webapp"):
+        run("git pull origin master")
+        run("bundle install")
+
+@task
+def status():
+    sudo("systemctl status --no-pager isuxi.ruby")
+
+@task
+def restart_ruby():
+    sudo("systemctl restart isuxi.ruby")
+
+@task
+def restart_nginx():
+    sudo("systemctl restart nginx")
+
+@task
+def deploy():
+    pull()
+    restart_ruby()
+    restart_nginx()
+
+@task
+def hello():
+    run("echo hello")
