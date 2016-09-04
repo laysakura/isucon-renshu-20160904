b6f1e467b948ad5e8e7ab85cc8a2bc1a443ff9ca
Yasuharu Sawada <yasuharu519@gmail.com>
2016-09-04 18:26:17 +0900

    no log

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -4,5 +4,5 @@ listen "/home/isucon/unicorn.sock", backlog: 1024
 listen 8080
 pid "/tmp/unicorn.pid"
 
-stderr_path "logs/unicorn.stderr.log"
-stdout_path "logs/unicorn.stdout.log"
+# stderr_path "logs/unicorn.stderr.log"
+# stdout_path "logs/unicorn.stdout.log"
